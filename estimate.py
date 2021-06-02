c83cc918109adaeffb55defa39ab81b4cbad1135
nissirose <84775997+nissirose@users.noreply.github.com>
2021-06-02 17:32:09 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -29,4 +29,63 @@ class TestMC(unittest.TestCase):
         
     
 if __name__ == "__main__":
+    unittest.main()import math
+import unittest
+import random
+class TestWallis(unittest.TestCase):
+    def test_low_iters(self):
+        for i in range(0, 5):
+            pi = wallis(i)
+            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
+            
+    def test_high_iters(self):
+        for i in range(500, 600):
+            pi = wallis(i)
+            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+
+
+class TestMC(unittest.TestCase):
+    def test_randomness(self):
+        pi0 = monte_carlo(15000)
+        pi1 = monte_carlo(15000)  self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
+
+        self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
+
+    def test_accuracy(self):
+        for i in range(500, 600):
+            pi = monte_carlo(i)
+            self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+        
+
+
+def monte_carlo(total):
+  inside = 0
+  for i in range(0, total):
+    x2 = random.random()**2
+    y2 = random.random()**2
+    if math.sqrt(x2 + y2) < 1.0:
+      inside += 1
+  pi = (float(inside) / total) * 4
+  return pi
+  
+print(monte_carlo(15000))
+
+
+
+def wallis(n):
+    pi = 0.0   
+    for i in range(1, n):
+        x = 4 * (i ** 2)
+        y = x - 1
+        z = float(x) / float(y)
+        if (i == 1):
+            pi = z
+        else:
+            pi *= z
+    pi *= 2
+    return pi
+
+print(wallis(15000))
+
+if _name_ == "_main_":
     unittest.main()
